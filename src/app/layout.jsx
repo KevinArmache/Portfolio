eeac5a39607aaf3900bbf80664f065374d124a10
KevinArmache <kevinarmache@gmail.com>
2023-12-14 14:28:52 +0100

    fix: add police of nier automata

---
+++ src/app/layout.jsx
@@ -0,0 +1,24 @@
+import { Inter } from "next/font/google";
+import NavBar from "./components/NavBar/NavBar";
+import "./globals.css";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export const metadata = {
+  title: "Kevin Armache Portfolio",
+  description:
+    "Dear human, this website is the portfolio of the developer Kevin Armache",
+};
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en">
+      <body className={inter.className}>
+        <header>
+          <NavBar />
+        </header>
+        {children}
+      </body>
+    </html>
+  );
+}
