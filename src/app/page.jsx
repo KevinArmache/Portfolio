f5e83de272d051f615a18c198d1150ae566fd509
KevinArmache <kevinarmache@gmail.com>
2023-12-20 16:28:37 +0100

    fix: size text home page

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -13,7 +13,7 @@ export default function Home() {
         Glory To Mankind
       </h1>
       <div className="text-center">
-        <h3 className={` ${rodin.className} text-center text-2xl `}>
+        <h3 className={` ${rodin.className} text-center text-3xl `}>
           Welcome{" "}
           <span className="bg-secondary-color p-1 font-bold text-primary-color">
             human
@@ -26,8 +26,8 @@ export default function Home() {
           <span className="bg-last-color mx-2  h-5 w-5"></span>
           <span className="text-primary-color font-bold">SYSTEM</span>
         </div>
-        <div className="bg-last-color p-2 text-center font-bold border border-secondary-color">
-          <p className="leading-loose">
+        <div className="bg-last-color p-2 text-center  font-bold border border-secondary-color">
+          <p className="leading-loose text-2xl">
             Hi human, this site is the presentation of the portfolio of the
             human who is represented in the name of Kevin Armache, who is the
             author of the creation of this website in the following sections we
@@ -35,7 +35,7 @@ export default function Home() {
             is called web development which is an area related to the creation
             of websites and web applications through programming languages.
           </p>
-          <p className="leading-loose">
+          <p className="leading-loose text-2xl">
             you will also see finished projects that Kevin has already had to
             realize for the moment I let you discover the portfolio, good visit
             dear human.
