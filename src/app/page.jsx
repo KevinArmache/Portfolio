8305a6e3100f1b63629a3801562f6b27ebde6d1e
KevinArmache <kevinarmache@gmail.com>
2023-12-20 16:51:40 +0100

    add: some text

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,4 +1,5 @@
 import localFont from "next/font/local";
+import Link from "next/link";
 const rodin = localFont({
   // src: "./utils/fonts/Rodin-Font/FOT-Rodin Pro DB.otf",
   src: "./utils/fonts/Rodin-Font/FOT-RodinPro-Bold.otf",
@@ -41,6 +42,33 @@ export default function Home() {
             dear human.
           </p>
         </div>
+      </div>
+
+      <div className="mx-auto text-center  w-2/3">
+        <Link href="">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="80"
+            height="80"
+            viewBox="0 0 24 24"
+            className="mx-auto mt-10"
+          >
+            <g
+              fill="none"
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              strokeWidth="2"
+            >
+              <path d="M0 0h24v24H0z" />
+              <path
+                fill="#4e4b42"
+                d="M19 2a3 3 0 0 1 3 3v14a3 3 0 0 1-3 3H5a3 3 0 0 1-3-3V5a3 3 0 0 1 3-3zm-6.387 5.21a1 1 0 0 0-1.32.083l-.083.094a1 1 0 0 0 .083 1.32L13.585 11H8l-.117.007A1 1 0 0 0 8 13h5.585l-2.292 2.293l-.083.094a1 1 0 0 0 1.497 1.32l4-4l.073-.082l.074-.104l.052-.098l.044-.11l.03-.112l.017-.126L17 12l-.007-.118l-.029-.148l-.035-.105l-.054-.113l-.071-.111a1.008 1.008 0 0 0-.097-.112l-4-4z"
+              />
+            </g>
+          </svg>
+        </Link>
+        <div className="h-px bg-black w-full mt-5 mb-10"></div>
+        <p>This website was inspired by the game Nier Automata</p>
       </div>
     </main>
   );
