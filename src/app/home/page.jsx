1a60b9deed5cd799f69d1c7ced25d7d3cc7ead55
KevinArmache <kevinarmache@gmail.com>
2023-12-31 14:43:05 +0100

    add: add introduction on home page

--- src/app/home/page.jsx
+++ src/app/home/page.jsx
@@ -1,9 +1,9 @@
-import React from "react";
+import Link from "next/link";
 
 export default function page() {
   return (
     <div className="flex ">
-      <div className="border-2 border-secondary-color w-1/2 mt-10">
+      <div className="w-1/2 mt-10">
         <div>
           <h2 className="text-secondary-color font-bold text-3xl border-b-4 border-secondary-color my-text-shadow w-fit mx-auto px-10 py-5">
             AN INTRODUCTION ABOUT HIM
@@ -17,7 +17,7 @@ export default function page() {
           </div>
           <div className="bg-last-color  text-center p-10  font-bold border border-secondary-color">
             <p className="leading-loose text-xl">
-              This human is called Kevin Armachehe is a full-stack javascript
+              This human is called Kevin Armache he is a full-stack Javascript
               developer he also has skills in UI & UX Design and he is
               constantly learning.
             </p>
@@ -28,10 +28,24 @@ export default function page() {
             </p>
             <p className="leading-loose text-xl">
               He is a passionate full stack software developer and he has
-              experience building web application withJavaScript, Reactjs,
+              experience building web application with JavaScript, Reactjs,
               Nodejs, and some other interesting libraries.
             </p>
           </div>
+        </div>
+        <div className="text-center w-3/5 mx-auto  m-10 flex">
+          <Link
+            href=""
+            className=" bg-secondary-color text-primary-color font-bold text-xl block p-5 w-1/2"
+          >
+            Check some project ?
+          </Link>
+          <Link
+            href={""}
+            className="border-2 transition duration-300 hover:bg-secondary-color  hover:text-primary-color text-secondary-color font-bold text-xl block p-5 border-secondary-color w-1/2"
+          >
+            Let's talk !
+          </Link>
         </div>
       </div>
       <div className="border-2 border-secondary-color w-1/2"></div>
